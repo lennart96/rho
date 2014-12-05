4c147eb6c0cc213570ec3ae1407894ef6de2df11
Lennart Landsmeer <lennart_96@hotmail.com>
2014-12-05 22:50:27 +0000

    reduce structures

--- Eval.hs
+++ Eval.hs
@@ -4,20 +4,32 @@ import Control.Monad (liftM2)
 
 import Expr
 
+isStk :: Expr -> Bool
+isStk Stk = True
+isStk _   = False
+
 reduce :: Expr -> Expr
 reduce t@(Var _)                = t
 reduce t@(Rho _ _)              = t
 reduce t@Stk                    = t
-reduce t@(Struc _)              = t
 reduce t@(Con _ _)              = t
+reduce t@(Struc es)             = t -- reduceStruc es
 reduce   (App e e')             = reduceApp e e'
+
+reduceStruc :: [Expr] -> Expr
+reduceStruc []                  = Stk
+reduceStruc [e]                 = e
+reduceStruc es = case filter isStk es of
+            []                  -> Stk
+            [e']                -> e'
+            es'                 -> Struc es'
 
 reduceApp :: Expr -> Expr -> Expr
 reduceApp Stk _                 = Stk
 reduceApp e@(Var _) e'          = App e e'
 reduceApp e@(App (Var _) _) e'  = App e e'
 reduceApp (App a b) c           = reduceApp (reduceApp a b) c
-reduceApp (Struc es) e          = Struc (map (`reduceApp` e) es)
+reduceApp (Struc es) e          = reduceStruc $ map (`reduceApp` e) es
 reduceApp (Con e es) e'         = Con e (es ++ [reduce e'])
 reduceApp (Rho pat e) e'        = maybe Stk
                                 ( reduce . flip (foldl (.) id . map substitute) e)
